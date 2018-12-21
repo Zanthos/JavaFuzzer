a4bd2fc564a3626db05c8e467774d4398968d767
Johnny Rockett <jtrocket@ncsu.edu>
2018-12-21 17:02:31 -0500

    Initial Commit

---
+++ java_fuzzer/src/main/java/rng_fuzzing/java_fuzzer/Fuzz.java
@@ -0,0 +1,11 @@
+package rng_fuzzing.java_fuzzer;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Fuzz {
+}
