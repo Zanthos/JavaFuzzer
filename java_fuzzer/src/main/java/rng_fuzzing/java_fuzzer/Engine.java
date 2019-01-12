0c55b2a5043d13fc26bf59522a0b46c840e01750
Johnny Rockett <jtrocket@ncsu.edu>
2019-01-12 14:14:07 -0500

    updateAndRunJar.sh now produces code coverage report with jacoco

--- java_fuzzer/src/main/java/rng_fuzzing/java_fuzzer/Engine.java
+++ java_fuzzer/src/main/java/rng_fuzzing/java_fuzzer/Engine.java
@@ -6,6 +6,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.knowm.xchart.RadarChart;
+import org.knowm.xchart.RadarChartBuilder;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
 import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
@@ -18,7 +22,7 @@ import rng_fuzzing.java_fuzzer.Fuzz;
  *
  */
 public class Engine {
-	@SuppressWarnings("rawtypes")
+	@SuppressWarnings({ "rawtypes", "unchecked" })
 	public static void run(String[] args) {
 		Reflections reflections = new Reflections(new ConfigurationBuilder()
 				.setUrls(ClasspathHelper.forPackage(args[0])).setScanners(new MethodAnnotationsScanner()));
@@ -45,5 +49,32 @@ public class Engine {
 				runner.start();
 			}
 		}
+
+		// Create Chart
+//		RadarChart chart = new RadarChartBuilder().width(500).height(500).title("Performance Breakdown").build();
+//		
+//		chart.setVariableLabels(
+//	        new String[] {
+//	          "Duration",
+//	          "Code Coverage",
+//	          "Crashes Found",
+//	          "Timeouts Found",
+//	          "Originality of Crashes",
+//	          "Originality of Timeouts"
+//	        });
+//		
+//		chart.addSeries("Monte Carlo", new double[] { 0.2, 1.0, 0.25, 0.44, 0.85, 0.19 });
+//		chart.addSeries("Mutation Based", new double[] { 0.8, 0.79, 0.25, 0.44, 0.85, 0.19 });
+//		chart.addSeries("Generation Based", new double[] { 0.52, 0.60, 0.74, 0.94, 0.26, 0.27 });
+//		chart.addSeries("My own strategy", new double[] { 0.0, 1.0, 1.0, 1.0, 1.0, 1.0 });
+//		
+//		// Show it
+//		new SwingWrapper(chart).displayChart();
+
+//		// Save it
+//		BitmapEncoder.saveBitmap(chart, "./Sample_Chart", BitmapFormat.PNG);
+//
+//		// or save it in high-res
+//		BitmapEncoder.saveBitmapWithDPI(chart, "./Sample_Chart_300_DPI", BitmapFormat.PNG, 300);
 	}
 }
