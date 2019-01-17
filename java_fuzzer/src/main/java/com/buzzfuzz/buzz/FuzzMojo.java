c57c1c5a66d2eaafd9117e4efaa12d2d03e2ed03
Johnny Rockett <jtrocket@ncsu.edu>
2019-01-16 23:49:09 -0500

    ClassLoading from .class files instead of jar so it can run in the testing phase

--- java_fuzzer/src/main/java/com/buzzfuzz/buzz/FuzzMojo.java
+++ java_fuzzer/src/main/java/com/buzzfuzz/buzz/FuzzMojo.java
@@ -5,8 +5,10 @@ import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.List;
 import java.util.Set;
 
+import org.apache.maven.artifact.DependencyResolutionRequiredException;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -26,43 +28,65 @@ import com.buzzfuzz.buzztools.Fuzz;
  * 
  * @author Rockett
  */
-@Mojo( name = "integration-test", defaultPhase = LifecyclePhase.INTEGRATION_TEST, requiresDependencyResolution = ResolutionScope.COMPILE )
+@Mojo( name = "test", defaultPhase = LifecyclePhase.TEST, requiresDependencyResolution = ResolutionScope.COMPILE )
 public class FuzzMojo  extends AbstractMojo
 {
 	
 	@Parameter( defaultValue = "${project}", readonly = true, required = true )
 	private MavenProject mavenProject;
 	
+	@Parameter(defaultValue = "${project.build.directory}")
+    private String projectBuildDir;
+	
 	public void execute() throws MojoExecutionException
     {
-		File jar = mavenProject.getArtifact().getFile();
+//		File file = mavenProject.getArtifact().getFile();
+		List<String> srcRoots = null;
+		try {
+			srcRoots = mavenProject.getCompileClasspathElements();
+		} catch (DependencyResolutionRequiredException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+//		List<?> srcRoots = mavenProject.getCompileSourceRoots();
+		URL[] urls = new URL[srcRoots.size()];
+		for (int i=0; i < srcRoots.size(); i++) {
+			try {
+				urls[i] = (new File(srcRoots.get(i))).toURI().toURL();
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+//		File file = new File(projectBuildDir);
 		
-		if (jar == null)
-			throw new MojoExecutionException("No jar found. Make sure that a jar is built before running this goal directly.");
+//		if (jar == null)
+//			throw new MojoExecutionException("No jar found. Make sure that a jar is built before running this goal directly.");
 		
 		Set<Method> methods = null;
 		Reflections reflections = null;
 		
-		try {
-			URLClassLoader child = new URLClassLoader(
-			        new URL[] {jar.toURI().toURL()},
-			        this.getClass().getClassLoader()
-			);
-			reflections = new Reflections(new ConfigurationBuilder().setUrls(
-					ClasspathHelper
-					.forClassLoader(child))
-					.addClassLoader(child)
-					.addScanners(
-							new MethodAnnotationsScanner(),
-							new SubTypesScanner(false)));
-			methods = reflections.getMethodsAnnotatedWith(Fuzz.class);
-			for (Method meth : methods) {
-				System.out.println(meth.getName());
-			}
-
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		//			URLClassLoader child = new URLClassLoader(
+		//			        new URL[] {jar.toURI().toURL()},
+		//			        this.getClass().getClassLoader()
+		//			);
+		//			System.out.println(file.toURI().toURL());
+		for (URL url : urls) {
+			System.out.println(url.toString());
+		}
+		URLClassLoader child = new URLClassLoader(
+				urls, 
+				this.getClass().getClassLoader());
+		reflections = new Reflections(new ConfigurationBuilder().setUrls(
+				ClasspathHelper
+				.forClassLoader(child))
+				.addClassLoader(child)
+				.addScanners(
+						new MethodAnnotationsScanner(),
+						new SubTypesScanner(false)));
+		methods = reflections.getMethodsAnnotatedWith(Fuzz.class);
+		for (Method meth : methods) {
+			System.out.println(meth.getName());
 		}
 		
 		Engine.run(methods, reflections);
